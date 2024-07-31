9cfd7806b323744db1cdd84b74d612f50a06ce41
Mydeen <Mydeen1193@gmail.com>
2024-07-31 19:10:13 +0530

    University
    University with Multipages

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var navLinks = document.getElementById("navLinks");
+
+function ShowMenu(){
+    navLinks.style.right = "0";
+}
+function HideMenu(){
+    navLinks.style.right = "-350px";
+}
+var layer = document.getElementsByClassName("layer") ;
+
+function raise(){
+    layer.style.top="0";
+}
+var submit = Document.getElementById("submit2") ;
+ function Thankyou(){
+    submit.style.display = "flex";
+    submit.style.display = "block";
+ }
